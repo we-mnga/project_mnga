2d91d771214c00b93459c774a16184276527b1d6
we_mnga <mnga.main@gmail.com>
2024-11-26 05:38:21 +0545

    added documentation

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,5 +1,5 @@
-from src.block import Block
-from src.proof_of_work import ProofOfWork
+from block import Block
+from proof_of_work import ProofOfWork
 
 class Blockchain:
     def __init__(self, difficulty=4):
@@ -9,10 +9,26 @@ class Blockchain:
         self.create_genesis_block()
 
     def create_genesis_block(self):
+        """
+        Creates and adds the genesis block to the blockchain.
+
+        The genesis block is the first block in the blockchain and has special properties:
+        - Its index is 0
+        - Its previous hash is "0"
+        - It contains no transactions
+        - Its timestamp is 0
+
+        This method initializes the blockchain by creating the genesis block,
+        computing its proof of work, and adding it to the chain.
+
+        Returns:
+            None
+        """
         genesis_block = Block(0, "0", [], 0)
         genesis_block.proof = ProofOfWork.compute_proof_of_work(0, self.difficulty)
         genesis_block_hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
+
 
     def add_transaction(self, sender, recipient, amount):
         transaction = {
@@ -32,4 +48,18 @@ class Blockchain:
 
     @property
     def last_block(self):
+        """
+    Retrieves the most recent block in the blockchain.
+
+    This property dynamically returns the last block added to the chain, which is
+    necessary for operations like mining a new block or validating the chain.
+
+    The use of @property allows it to be accessed like an attribute, improving readability
+    and encapsulating the logic for fetching the last block.
+
+    Returns:
+        Block: The last block in the blockchain.
+    """
         return self.chain[-1]
+
+
